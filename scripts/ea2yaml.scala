4c6520321ff3454df30a83ea98d3aebf6021264a
carlos_martin_garcia <carlos.martin@adesis.com>
2016-04-24 12:50:27 +0200

    xml file processing basics

---
+++ scripts/ea2yaml.scala
@@ -0,0 +1,30 @@
+/**
+  * Created by cmartin on 24/04/16.
+  */
+import xml.XML.loadFile
+import scala.xml.Node
+
+val xmlFilePath = "resources/ea-classes.xml"
+
+// process node
+/*
+def processNode(node: Node): String =
+  node match {
+    case <xs:element{attributes}/> => "it's an element with attributes: " + attributes
+    case _ => "Not recognized"
+}
+*/
+
+// read xml file from disk
+val xmlFile = loadFile(xmlFilePath)
+//println(xmlFile)
+
+val elements = xmlFile \ "complexType"
+val elementsCount = elements.size
+println(f"elements has $elementsCount elements")
+
+for (element <- elements) {
+  println(element \ "@name")
+  val attributes = element \\ "element" \\ "@name"
+  attributes.map(a => println(f" -$a"))
+}
