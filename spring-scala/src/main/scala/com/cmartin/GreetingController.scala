5fd6e223d788cca9d63fa66f2c6a70d359b1fe91
butcherless <cmarting@gmail.com>
2017-10-13 10:06:20 +0200

    chore: update dependencies

--- spring-scala/src/main/scala/com/cmartin/GreetingController.scala
+++ spring-scala/src/main/scala/com/cmartin/GreetingController.scala
@@ -11,12 +11,12 @@ import org.slf4j.{Logger, LoggerFactory}
 class GreetingController {
   private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-
   @RequestMapping(Array("/greeting"))
-  def greeting(@RequestParam(value = "name", required = false, defaultValue = "World") name: String, model: Model): String = {
+  def greeting(@RequestParam(value = "name", required = false, defaultValue = "donald")
+      name: String, model: Model): String = {
     logger.debug(s"name: ${name}")
     //model.addAttribute("name", name)
-    val person = new Person(1, name, "lastName", s"${name}@domain.com")
+    val person = new Person(1, name, "lastName", s"${name}@duck.com")
     model.addAttribute("name", person.firstName)
     model.addAttribute("email", person.email)
     model.addAttribute("date", LocalDateTime.now())
