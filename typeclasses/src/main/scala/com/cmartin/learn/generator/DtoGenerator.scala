63af5a8636de07e991e9d9491557aab6a493357d
butcherless <cmarting@gmail.com>
2017-12-04 23:46:57 +0100

    feat: simple implementations of algebra

--- typeclasses/src/main/scala/com/cmartin/learn/generator/DtoGenerator.scala
+++ typeclasses/src/main/scala/com/cmartin/learn/generator/DtoGenerator.scala
@@ -1,23 +1,47 @@
 package com.cmartin.learn.generator
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 trait DtoGenerator[P[_]] {
   def create(name: Type): P[TypeRepresentation]
 
-  def addProperty(prop: Property, t: Type): P[TypeRepresentation]
+  def addProperty(prop: Property): P[TypeRepresentation]
 
   def addGetters(t: Type): P[TypeRepresentation]
 
-  def addToString(builder: ToStringBuilder, t: Type): P[TypeRepresentation]
+  def addToString(builder: ToStringBuilder): P[TypeRepresentation]
 }
 
 object TryDtoGenerator extends DtoGenerator[Try] {
-  override def create(name: Type) = ???
+  override def create(name: Type) = {
+    Try(TypeRepresentation(Type("pkg", "name"), File("dir", "name")))
+  }
 
-  override def addProperty(prop: Property, t: Type) = ???
+  override def addProperty(prop: Property) = ???
 
   override def addGetters(t: Type) = ???
 
-  override def addToString(builder: ToStringBuilder, t: Type) = ???
+  override def addToString(builder: ToStringBuilder) = ???
+}
+
+object OptionDtoGenerator extends DtoGenerator[Option] {
+  override def create(name: Type) = {
+    Option(TypeRepresentation(Type("pkg", "name"), File("dir", "name")))
+  }
+
+  override def addProperty(prop: Property) = ???
+
+  override def addGetters(t: Type) = ???
+
+  override def addToString(builder: ToStringBuilder) = ???
+}
+
+object Main extends App {
+  println("DtoGenerator algebra!")
+  val typeRep = TryDtoGenerator.create(Type("mypkg", "MyTpe"))
+  typeRep match {
+    case Success(s) => println(s"TypeRepresentation[type: ${s.t}, file: ${s.f}]")
+    case Failure(f) => println(s"function create type " + f.toString)
+  }
+  
 }
