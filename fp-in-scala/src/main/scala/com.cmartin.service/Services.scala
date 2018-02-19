e8d9d6e2f91aaa3010e0f87e2147619255b256dc
butcherless <cmarting@gmail.com>
2018-02-19 23:07:58 +0100

    feat: data refactor; service composition for comprehension

--- fp-in-scala/src/main/scala/com.cmartin.service/Services.scala
+++ fp-in-scala/src/main/scala/com.cmartin.service/Services.scala
@@ -2,49 +2,52 @@ package com.cmartin.service
 
 import scala.collection.immutable.HashMap
 
-case class Color(name: String)
+case class Color(name: String, next: Int)
 
 case class Shape(id: Int, name: String, area: Double)
 
 case class Fruit(id: Int, name: String, color: Color)
 
 trait AbstractService[T] {
-  def getByHash(h: Int): T
+  def getByHash(h: Int): Option[T]
 }
 
 class ColorServiceImpl(repo: ColorRepository) extends AbstractService[Color] {
-
-  override def getByHash(h: Int): Color = ???
+  override def getByHash(h: Int): Option[Color] = repo.getColorByNumber(h)
 }
 
+//TODO
 class ShapeServiceImpl extends AbstractService[Shape] {
-  override def getByHash(h: Int): Shape = ???
+  override def getByHash(h: Int): Option[Shape] = None // Some(Shape(1,"circle", math.Pi))
 }
 
+//TODO
 class FruitServiceImpl extends AbstractService[Fruit] {
-  override def getByHash(h: Int): Fruit = ???
+  override def getByHash(h: Int): Option[Fruit] = Some(Fruit(2, "apple", Color("yellow", 7)))
 }
 
 object Services {
   def calcHashId(t: Color): Int = math.abs(t.hashCode() % 10 + 1)
-
 }
 
 class ColorRepository {
   val colorNames = List("red", "yellow", "blue", "black", "white", "green", "grey", "pink", "brown", "cyan", "magenta")
 
-  private var colorMap = HashMap[String, Color]()
+  private var colorMap = HashMap[Int, Color]()
 
-  //  def createColors(): Unit = {
-  for (name <- colorNames) {
-    val color = Color(name)
-    colorMap += (name -> color)
+  for (i <- 1 to colorNames.length) {
+    val color = Color(colorNames(i - 1), i * 2 - 1)
+    colorMap += (i -> color)
   }
-  //  }
 
-  def getColorByName(n: String): Option[Color] = {
+  def getColorByNumber(n: Int): Option[Color] = {
     colorMap.get(n)
   }
 
   def colorCount(): Int = colorMap.size
+
+  def isEmpty(): Boolean = colorNames.isEmpty
+
 }
+
+//TODO ShapeRepository & FruitRepository
--- fp-in-scala/src/test/scala/com/cmartin/service/ServicesSpec.scala
+++ fp-in-scala/src/test/scala/com/cmartin/service/ServicesSpec.scala
@@ -4,16 +4,44 @@ import org.specs2.mutable.Specification
 
 class ServicesSpec extends Specification {
 
-  "should generate an Int between 1 and 10 for Color class" >> {
-    val color = Color("red")
+  val RED = "red"
+  val LIGHT_RED = "light-red"
+
+  "hash function should generate an Int between 1 and 10 for Color class" >> {
+    val color = Color("red", 1)
     val res = Services.calcHashId(color)
     res must beGreaterThanOrEqualTo(1) and beLessThanOrEqualTo(10)
   }
 
   "color repository should not be empty" >> {
     val repo = new ColorRepository
-    val res = repo.colorCount()
-    res must not be equalTo(0)
+    repo.isEmpty() must beFalse
   }
 
+  "color with value 1 should exist in the repository" >> {
+    val repo = new ColorRepository
+    repo.getColorByNumber(1).isDefined must beTrue
+  }
+
+  "color with value 100 should not exist in the repository" >> {
+    val repo = new ColorRepository
+    repo.getColorByNumber(100).isDefined must beFalse
+  }
+
+
+  "service compositior" >> {
+    val colorService = new ColorServiceImpl(new ColorRepository)
+    val shapeService = new ShapeServiceImpl
+    val fruitService = new FruitServiceImpl
+
+    //TODO refactor for-comprehension in a funtion f(x: Int) : Option[Fruit)
+
+    val fruit = for {
+      color <- colorService.getByHash(1)
+      shape <- shapeService.getByHash(color.next)
+      fruit <- fruitService.getByHash(shape.id)
+    } yield fruit
+
+    fruit must beNone
+  }
 }
