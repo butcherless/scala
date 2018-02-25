5648776ca8054dfd58efb36dd1c801026e9c6d81
butcherless <cmarting@gmail.com>
2018-02-25 13:37:02 +0100

    test refactor

--- fp-in-scala/src/test/scala/com/cmartin/dto/DtoSpec.scala
+++ fp-in-scala/src/test/scala/com/cmartin/dto/DtoSpec.scala
@@ -2,25 +2,23 @@ package com.cmartin.dto
 
 import org.specs2.mutable.Specification
 
-class ServicesSpec extends Specification {
+class DtoSpec extends Specification {
   val ID = 1234
   val REGISTRATION = "EC-LXM"
   val MODEL = "350-800"
+  val EMPTY_STRING = ""
 
   "mandatory constructor arguments" >> {
-    val plane = Plane(ID, REGISTRATION)
-    val t1 = (plane.id, plane.registration)
-    val resTuple = (ID, REGISTRATION)
-    resTuple must beEqualTo(t1)
-    plane.brand.isEmpty must beTrue
-    plane.model.isEmpty must beTrue
+    val tuple = plane2Tuple(Plane(ID, REGISTRATION))
+    val resTuple = (ID, REGISTRATION, EMPTY_STRING, EMPTY_STRING)
+    resTuple must beEqualTo(tuple)
   }
 
   "named constructor arguments" >> {
-    val plane = Plane(ID, REGISTRATION, model = MODEL)
-    val t1 = (plane.id, plane.registration, plane.model)
-    val resTuple = (ID, REGISTRATION, MODEL)
-    resTuple must beEqualTo(t1)
+    val tuple = plane2Tuple(Plane(ID, REGISTRATION, model = MODEL))
+    val resTuple = (ID, REGISTRATION, EMPTY_STRING, MODEL)
+    resTuple must beEqualTo(tuple)
   }
 
+  def plane2Tuple(p: Plane) = Plane.unapply(p).get
 }
