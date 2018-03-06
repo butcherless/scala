e9d9a3cfcf40be85b8c428ec2ef94d47779421c5
butcherless <cmarting@gmail.com>
2018-03-06 23:50:55 +0100

    add scala option return type

--- spring-scala/src/main/java/com/cmartin/learn/demoapp/repository/PersonRepository.java
+++ spring-scala/src/main/java/com/cmartin/learn/demoapp/repository/PersonRepository.java
@@ -2,7 +2,10 @@ package com.cmartin.learn.demoapp.repository;
 
 import com.cmartin.learn.demoapp.domain.User;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import scala.Option;
 
 public interface PersonRepository extends Neo4jRepository<User, Long> {
     User findByEmail(String email);
+
+    Option<User> findByFirstName(String firstName);
 }
