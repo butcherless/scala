b76d751fb3af16c1f754161ffdd744aeeb905c91
butcherless <cmarting@gmail.com>
2018-03-29 02:44:31 +0200

    logic module cobertura

--- dep-analyzer/src/test/scala/com/cmartin/utils/DepAnalyzerSpec.scala
+++ dep-analyzer/src/test/scala/com/cmartin/utils/DepAnalyzerSpec.scala
@@ -1,26 +1,28 @@
 package com.cmartin.utils
 
-import com.cmartin.utils.Logic.{Dep, getDependency}
+import com.cmartin.utils.Logic._
 import org.specs2.mutable.Specification
+
+import scala.collection.SortedSet
 
 class DepAnalyzerSpec extends Specification {
 
   val dep1 = Dep("dep.group", "dep-artifact", "dep-version-1")
   val dep2 = Dep("dep.group", "dep-artifact", "dep-version-2")
 
-  "empty string" >> {
+  "get dependency empty string" >> {
     val s = "non-dependency-line"
     val result = getDependency(s)
     result must beNone
   }
 
-  "dependency match" >> {
+  "get dependency string  match" >> {
     val s = "+--- org.springframework.boot:spring-boot-starter-web -> 1.5.10.RELEASE (*)"
     val result = getDependency(s)
     result must beSome
   }
 
-  "Dep key must be group+artifact" >> {
+  "dependency key must be group+artifact" >> {
     val group = "org.springframework.boot"
     val artifact = "spring-boot-starter-web"
     val version = "1.5.10.RELEASE"
@@ -37,9 +39,23 @@ class DepAnalyzerSpec extends Specification {
     val result = Dep.ord.compare(dep2, dep1)
     result must beGreaterThan(0)
   }
+
   "Dependency comparator equals to" >> {
     val result = Dep.ord.compare(dep1, dep1)
     result must beEqualTo(0)
+  }
+
+  "mkString formatter string should contain dependency key" >> {
+    val key = "dependency-key"
+    val set: SortedSet[Dep] = SortedSet[Dep]()
+    val result = mkString(key, set)
+    result must contain(key)
+  }
+
+  "mkErrorString formatter string should contain parameter" >> {
+    val message = "error-message"
+    val result = mkErrorString(message)
+    result must contain(message)
   }
 
 }
