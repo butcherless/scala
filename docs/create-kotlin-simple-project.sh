1ab1b08338b97696ea80033efc39ed26cf701f8c
butcherless <cmarting@gmail.com>
2018-05-25 12:42:55 +0200

    basic kotlin app script

--- docs/create-kotlin-simple-project.sh
+++ docs/create-kotlin-simple-project.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PKG=com.cmartin.learn
 PKG_DIR=com/cmartin/learn
 KOTLIN_VERSION=1.2.41
 JUNIT5_VERSION=5.2.0
@@ -7,10 +8,9 @@ JUNIT_PLATFORM_VERSION=1.2.0
 ASSERTJ_VERSION=3.9.1
 
 # create filesystem
-mkdir -p project src/{main,test}/{resources,scala} src/main/scala/${PKG_DIR} src/test/scala/${PKG_DIR}
+mkdir -p project src/{main,test}/{resources,kotlin} src/{main,test}/kotlin/${PKG_DIR}
 
-echo '
-buildscript {
+echo 'buildscript {
     ext.kotlin_version = "'${KOTLIN_VERSION}'"
     ext.junit_version = "'${JUNIT5_VERSION}'"
     ext.junit_platform_version = "'${JUNIT_PLATFORM_VERSION}'"
@@ -22,13 +22,16 @@ buildscript {
 
     dependencies {
         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlin_version}"
-        classpath "org.junit.platform:junit-platform-gradle-plugin:${junit_platform_version}"
+        //classpath "org.junit.platform:junit-platform-gradle-plugin:${junit_platform_version}"
     }
 }
 
 apply plugin: "kotlin"
-apply plugin: "org.junit.platform.gradle.plugin"
+apply plugin: "application"
+//apply plugin: "org.junit.platform.gradle.plugin"
 apply plugin: "jacoco"
+
+mainClassName = "'${PKG}'.HelloKt"
 
 repositories {
     mavenCentral()
@@ -43,8 +46,20 @@ dependencies {
 
     testRuntime "org.junit.jupiter:junit-jupiter-engine:${junit_version}"
 }
+
+test {
+    useJUnitPlatform()
+}
 ' > build.gradle
+
+echo 'package '${PKG}'
+
+fun main(args: Array<String>) {
+    println("Hello, Kotlin World!")
+}
+' > src/main/kotlin/${PKG_DIR}/hello.kt
 
 gradle wrapper --gradle-version 4.7
 rm -f gradlew.bat
 ./gradlew -v
+./gradlew clean test run
