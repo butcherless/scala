dc84c59ae8656c63d2fef00217a035a0138384d4
Carlos Martin <cmarting@gmail.com>
2018-07-17 22:57:52 +0200

    refactor code; add Message DTO; add custom JsonFormat (write)

--- akka-http-hw/README.md
+++ akka-http-hw/README.md
@@ -6,9 +6,9 @@
 - project akkahttphw
 - run
 - Test CRUD
-  - Create: http -v POST localhost:8080/transfer < src/test/resources/transfer.json
-  - Read: http -v GET localhost:8080/transfer/666
-  - Update: http -v PUT localhost:8080/transfer/666 < src/test/resources/transfer.json 
+  - Create: http -v POST localhost:8080/transfer < src/test/resources/transfer-dto.json
+  - Read: http -v GET localhost:8080/transfer/4e4387c4-38e0-4fd8-80cd-2ca7a6395d8e
+  - Update: http -v PUT localhost:8080/transfer/4e4387c4-38e0-4fd8-80cd-2ca7a6395d8e < src/test/resources/transfer.json 
   - Delete: http -v DELETE localhost:8080/transfer/4e4387c4-38e0-4fd8-80cd-2ca7a6395d8e
  
 
--- akka-http-hw/src/main/scala/com/cmartin/route/package.scala
+++ akka-http-hw/src/main/scala/com/cmartin/route/package.scala
@@ -1,5 +1,6 @@
 package com.cmartin
 
+import java.time.LocalDateTime
 import java.util.UUID
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
@@ -7,7 +8,7 @@ import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse}
 import akka.http.scaladsl.server.Directives
 import org.slf4j.LoggerFactory
-import spray.json.DefaultJsonProtocol
+import spray.json.{DefaultJsonProtocol, JsObject, JsString, JsValue, RootJsonFormat}
 
 import scala.util.Random
 
@@ -18,15 +19,30 @@ package object route {
   val CURRENCY = "EUR"
   val HELLO_MESSAGE = "hello from akka http"
   val BYE_MESSAGE = "bye from akka http"
+  val SOURCE_ACCOUNT = "20950230509755438646"
+  val TARGET_ACCOUNT = "01822348985742908767"
 
   val logger = LoggerFactory.getLogger("route")
 
   // domain model
+  final case class Message(text: String, dateTime: LocalDateTime)
+
   final case class Transfer(source: String, target: String, amount: BigDecimal, currency: String, id: String)
+
 
   // JSON Format, Marshaller & Unmarshaller
   trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
     implicit val transferFormat = jsonFormat5(Transfer)
+
+    implicit object MessageJsonFormat extends RootJsonFormat[Message] {
+      override def read(json: JsValue): Message = ???
+
+      override def write(message: Message) = JsObject(
+        "dateTime" -> JsString(message.dateTime.toString),
+        "text" -> JsString(message.text)
+      )
+    }
+
   }
 
   // controller
@@ -35,78 +51,81 @@ package object route {
     val HELLO = "hello"
     val BYE = "bye"
     val TRANSFER = "transfer"
+    val ID_REGEX = """[a-z0-9-]+""".r
   }
 
   class ApiController extends Directives with JsonSupport {
     val route =
-    /*      path(ControllerPath.HELLO) {
-            get {
-              logger.debug("hello.in")
-              // logic goes here
-              logger.debug(s"hello.out: $HELLO_MESSAGE")
+      path(ControllerPath.HELLO) {
+        get {
+          logger.debug("hello.in")
+          // logic goes here
+          logger.debug(s"hello.out: $HELLO_MESSAGE")
 
-              complete(buildTextResponse(OK.intValue, HELLO_MESSAGE))
-            }
-          } ~
-            path(ControllerPath.BYE) {
-              get {
-                complete(buildTextResponse(OK.intValue, BYE_MESSAGE))
-              }
-             } ~ */
-      pathPrefix(ControllerPath.TRANSFER) {
-        pathEnd {
-          post {
-            entity(as[Transfer]) { t =>
-              logger.debug(s"post.in: $t")
-              complete {
-                val entity = t.copy(id = getId())
-                buildTextResponse(Created.intValue, s"Entity created ${entity}")
-              }
-            }
+          complete(OK.intValue, buildMessage(HELLO_MESSAGE))
+        }
+      } ~
+        path(ControllerPath.BYE) {
+          get {
+            complete(OK.intValue, buildMessage(BYE_MESSAGE))
           }
         } ~
-          path(IntNumber) { id =>
-            get {
-              logger.debug(s"get.in: $id")
-              val transfer = buildTransfer()
-              logger.debug(s"transfer.out: $transfer")
+        pathPrefix(ControllerPath.TRANSFER) {
+          pathEnd {
+            post {
+              entity(as[Transfer]) { dto =>
+                logger.debug(s"post.in: $dto")
+                val entity = dto.copy(id = getId())
 
-              complete(buildTransfer())
+                complete(Created.intValue, entity)
+              }
             }
           } ~
-          path(IntNumber) { id =>
-            put {
-              entity(as[Transfer]) { t =>
+            path(ControllerPath.ID_REGEX) { id =>
+              get {
+                logger.debug(s"get.in: $id")
+                val transfer = buildTransfer()
+                logger.debug(s"transfer.out: $transfer")
 
-                complete {
+                complete(buildTransfer(id))
+              }
+            } ~
+            path(ControllerPath.ID_REGEX) { id =>
+              put {
+                entity(as[Transfer]) { t =>
                   logger.debug(s"put.in: $t")
                   val r = new Random().nextDouble()
                   val updated = t.copy(amount = t.amount * r)
                   logger.debug(s"transfer.out: $updated")
 
-                  buildTextResponse(OK.intValue, s"Amount updated to ${updated}")
+                  complete(OK.intValue, updated)
                 }
               }
-            }
-          } ~
-          path( """[a-z0-9-]+""".r) { id =>
-            delete {
-              complete {
+            } ~
+            path(ControllerPath.ID_REGEX) { id =>
+              delete {
                 logger.debug(s"delete.in: $id")
-                buildTextResponse(OK.intValue, s"Transfer deleted")
+                complete(OK.intValue, buildMessage(id))
               }
             }
-          }
-      }
+        }
   }
 
   def buildTextResponse(code: Int, message: String) = {
     HttpResponse(code, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, message))
   }
 
+  def buildJsonResponse(code: Int, message: String) = {
+    HttpResponse(code, entity = HttpEntity(ContentTypes.`application/json`, message))
+  }
+
   def getId() = UUID.randomUUID().toString
 
-  def buildTransfer(): Transfer = Transfer("20950230...1", "01822348...2", BigDecimal.apply(100.0), CURRENCY, getId())
+  def buildMessage(text: String) = Message(text, LocalDateTime.now)
+
+  def buildTransfer(): Transfer = buildTransfer(getId())
+
+  def buildTransfer(id: String): Transfer = Transfer(SOURCE_ACCOUNT, TARGET_ACCOUNT, BigDecimal.apply(100.0), CURRENCY, id)
 
 
 }
