2600ae1ad222bc3a9c1779a1d14ad7da6280cb21
Carlos Martin <cmarting@gmail.com>
2018-08-12 13:54:07 +0200

    added POC project API webapp

---
+++ webapp-api/common/src/main/scala/com/cmartin/learn/common.scala
@@ -0,0 +1,7 @@
+package com.cmartin.learn
+
+package object common {
+  val dummyCommonVal = 0
+
+  def sayHelloCommon() = "hello from common"
+}
---
+++ webapp-api/controller/src/main/scala/com/cmartin/learn/route.scala
@@ -0,0 +1,9 @@
+package com.cmartin.learn
+
+import com.cmartin.learn.common.sayHelloCommon
+
+package object route {
+  val dummyControllerVal = 0
+
+  val myVal = sayHelloCommon()
+}
