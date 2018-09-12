b01d300353e82e46c3cb30128a1ebdd2b1f4e23f
Admin <cmarting@gmail.com>
2018-09-13 01:00:46 +0200

    akka http read stream and simple transformation

--- akka-http-cl/src/main/scala/com/cmartin/WebClient.scala
+++ akka-http-cl/src/main/scala/com/cmartin/WebClient.scala
@@ -1,9 +1,13 @@
 package com.cmartin
 
+import java.io.File
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{FileIO, Framing}
+import akka.util.ByteString
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -15,13 +19,28 @@ object WebClient extends Greeting {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "https://akka.io"))
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://apache.org/"))
 
     responseFuture
       .onComplete {
-        case Success(res) => println(res)
+        case Success(response) => {
+          response.entity.dataBytes
+            .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 4096))
+            .map(transformEachLine)
+            .runWith(FileIO.toPath(new File("/tmp/example.out").toPath))
+          println(response.headers.foreach(println(_)))
+        }
         case Failure(_) => sys.error("something wrong")
       }
+
+    def transformEachLine(line: ByteString): ByteString = ByteString(line.utf8String.toUpperCase)
+
+
+    Thread.sleep(4000)
+    
+    println(s"isCompleted: ${responseFuture.isCompleted}")
+    materializer.shutdown()
+    system.terminate()
   }
 }
 
