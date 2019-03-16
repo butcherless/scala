a74ae2b9a3a86f531a4032865ba342f09835debb
Carlos Martin <cmarting@gmail.com>
2019-03-16 13:39:11 +0100

    refactor: remove unused code

--- refined-poc/src/test/scala/com/cmartin/learn/refined/RefinedSpec.scala
+++ refined-poc/src/test/scala/com/cmartin/learn/refined/RefinedSpec.scala
@@ -32,46 +32,6 @@ class RefinedSpec extends FlatSpec with Matchers {
 
     res.left.value.contains(Constants.PredicateFailed) shouldBe true
   }
-
-  it should "validate a well known port" in {
-    val a: Int = Constants.Port22
-    val res = validateWellKnownPort(a)
-
-    res.right.get.value shouldBe a
-  }
-
-  it should "reject a non well known port" in {
-    val expected = None
-    val a: Int = Constants.Port2222
-    val res = validateWellKnownPort(a)
-
-    res.toOption shouldBe expected
-  }
-
-  it should "validate an user port" in {
-    val a: Int = Constants.Port8080
-    val res = validateUserPort(a)
-
-    res.right.get.value shouldBe a
-  }
-
-  it should "reject a non user port" in {
-    val expected = None
-    val a: Int = Constants.Port22
-    val res = validateUserPort(a)
-
-    res.toOption shouldBe expected
-  }
-
-  it should "validate a network port" in {
-    val expectedPorts = List(Constants.Port22, Constants.Port2222)
-    val resList = expectedPorts map (validateNetworkPort(_))
-    val pairs = resList zip expectedPorts
-
-    resList forall (_.isRight) shouldBe true
-    pairs forall (p => p._1.right.get.value == p._2)
-  }
-
 }
 
 object Constants {
