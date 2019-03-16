af9863a19d87353683e6c0cf8e9a4538dc1c5c1f
Carlos Martin <cmarting@gmail.com>
2019-03-16 12:29:08 +0100

    added abstract property spec

--- refined-poc/src/test/scala/com/cmartin/learn/refined/LeapYearSpec.scala
+++ refined-poc/src/test/scala/com/cmartin/learn/refined/LeapYearSpec.scala
@@ -1,20 +1,17 @@
 package com.cmartin.learn.refined
 
-import org.scalatest.{Matchers, PropSpec}
-import org.scalatest.prop.TableDrivenPropertyChecks
-
-class LeapYearSpec extends PropSpec with TableDrivenPropertyChecks with Matchers {
+class LeapYearSpec extends PropertySpec {
 
   val leapYears = Table("year", 1600, 1992, 2000, 2020)
   val nonLeapYears = Table("year", 1700, 1800, 2002, 2100)
 
-  property("CURRENT leapYears should contains valid leap year values") {
+  property("leapYears should contains valid leap year values") {
     forAll(leapYears) {
       year => validateLeapYear(year).isRight shouldBe true
     }
   }
 
-  property("CURRENT nonLeapYears should contains invalid leap year values") {
+  property("nonLeapYears should contains invalid leap year values") {
     forAll(nonLeapYears) {
       year => validateLeapYear(year).isLeft shouldBe true
     }
---
+++ refined-poc/src/test/scala/com/cmartin/learn/refined/PropertySpec.scala
@@ -0,0 +1,7 @@
+package com.cmartin.learn.refined
+
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{Matchers, PropSpec}
+
+class PropertySpec extends PropSpec with TableDrivenPropertyChecks with Matchers {
+}
