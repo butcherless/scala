0bdc1eb5c358222758e6103fbcc69f8be30d6187
Carlos Martin <cmarting@gmail.com>
2019-04-01 16:18:57 +0200

    akka, initial examples

---
+++ akka-actors/src/main/scala/com/cmartin/learn/Main.scala
@@ -0,0 +1,20 @@
+package com.cmartin.learn
+
+import akka.actor.{ActorSystem, Props}
+import com.cmartin.learn.actors.{Consumer, Producer}
+
+import scala.concurrent.duration._
+
+object Main extends App {
+  val system = ActorSystem("IntegerCalc")
+
+  val producer = system.actorOf(Props[Producer], "producer")
+  val consumer = system.actorOf(Props(classOf[Consumer],producer), "consumer")
+
+  //producer ! Consumer.Success(3)
+  //producer ! Consumer.Failure("dummy error")
+
+    producer
+    //consumer ! Producer.Module(22)
+
+}
---
+++ akka-actors/src/main/scala/com/cmartin/learn/actors.scala
@@ -0,0 +1,56 @@
+package com.cmartin.learn
+
+import akka.actor.{Actor, ActorLogging, ActorRef, PoisonPill}
+import com.cmartin.learn.actors.Consumer.Success
+
+package object actors {
+
+
+  class Producer extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case Consumer.Success(mod) => {
+        log.info(s"module is: ${mod}")
+      }
+      case Consumer.Failure(e) => log.error(s"processing error: $e")
+
+      case Producer.Request(n) => 
+    }
+
+    def requestSequence(consumer: ActorRef) = {
+      consumer ! Producer.Module(22)
+    }
+  }
+
+  // companion object
+  object Producer {
+    case class Request(n: Int)
+    case class Module(n: Int)
+
+    case object Stop
+
+  }
+
+  class Consumer(producer: ActorRef) extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case Producer.Module(n) => {
+        producer ! Success(n % 10)
+        log.info(s"request module for number: ${n}")
+      }
+      case Producer.Stop => {
+        log.info(s"shutdown actor $self")
+        self ! PoisonPill
+      }
+
+    }
+  }
+
+  // companion object
+  object Consumer {
+
+    case class Success(n: Int)
+
+    case class Failure(message: String)
+
+  }
+
+}
