d1d18ebdd0f98f213410e1eb252c520fc9e15546
butcherless <cmarting@gmail.com>
2019-08-06 14:56:15 +0200

    chore: upgrade cats library

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val akkaHttpVersion = "10.1.9"
   lazy val akkaVersion = "2.5.23"
-  lazy val catsVersion = "2.0.0-M4"
+  lazy val catsVersion = "2.0.0-RC1"
   lazy val configVersion = "1.3.4"
   lazy val json4sVersion = "3.6.7"
   lazy val kafkaClientVersion = "2.3.0"
