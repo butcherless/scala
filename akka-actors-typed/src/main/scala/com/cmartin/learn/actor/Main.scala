e5ae08680ab4dd86068feff456fc10934b6dc517
Carlos Martin <cmarting@gmail.com>
2019-08-26 10:29:42 +0200

    typed actors pocs

--- akka-actors-typed/src/main/scala/com/cmartin/learn/actor/Main.scala
+++ akka-actors-typed/src/main/scala/com/cmartin/learn/actor/Main.scala
@@ -1,12 +1,36 @@
 package com.cmartin.learn.actor
 
-import akka.actor.typed.ActorSystem
-import scala.concurrent.duration._
-import scala.concurrent.Await
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
 
 object Main extends App {
-  // Create ActorSystem and top level supervisor
-  val system: ActorSystem[Nothing] = ActorSystem[Nothing](IotSupervisor(), "iot-system")
 
+  def delay(delay: Int) = {
+    var count = 0
+    val timeout = System.currentTimeMillis() + delay
+
+    while (System.currentTimeMillis() < timeout) {
+      count = count + 1
+    }
+    println(count)
+  }
+
+  // Create ActorSystem and top level supervisor
+  //val system: ActorSystem[Nothing] = ActorSystem[Nothing](IotSupervisor(), "iot-system")
+
+  //system.terminate()
+
+  val greeter: Behavior[String] =
+    Behaviors.receiveMessage[String] { message =>
+      println(s"Hello $message")
+      Behaviors.stopped
+    }
+
+  val system: ActorSystem[String] = ActorSystem(greeter, "helloworld")
+
+  system ! "Papuchi"
+
+  //StdIn.readLine()
+  delay(1000)
   system.terminate()
 }
