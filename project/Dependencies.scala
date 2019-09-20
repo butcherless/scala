2c939693b2d0ddd4630b1d0d7dc1e2811c13813a
Carlos Martin <cmarting@gmail.com>
2019-09-20 20:21:04 +0200

    upgrade dependencies and sbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val akkaHttpVersion = "10.1.9"
+  lazy val akkaHttpVersion = "10.1.10"
   lazy val akkaVersion = "2.5.25"
   lazy val akkaTypedVersion = "2.5.25"
   lazy val catsVersion = "2.0.0"
