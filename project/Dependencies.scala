34334833621f8c95badee4ca637054ad3d64b60e
butcherless <cmarting@gmail.com>
2019-10-09 10:23:03 +0200

    chore: upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val akkaVersion = "2.5.25"
   lazy val akkaTypedVersion = "2.5.25"
   lazy val catsVersion = "2.0.0"
-  lazy val circeVersion = "0.12.1"
+  lazy val circeVersion = "0.12.2"
   lazy val configVersion = "1.3.4"
   lazy val json4sVersion = "3.6.7"
   lazy val kafkaClientVersion = "2.3.0"
@@ -17,7 +17,7 @@ object Dependencies {
   lazy val scalatestVersion = "3.0.8"
   lazy val slf4jVersion = "1.7.26"
   lazy val specs2Version = "4.7.1"
-  lazy val sttpVersion = "1.7.0"
+  lazy val sttpVersion = "1.7.1"
   lazy val uTestVersion = "0.7.1"
   lazy val zioVersion = "1.0.0-RC14"
 
