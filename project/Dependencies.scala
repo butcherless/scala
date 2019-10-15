6234a92b0d43bfa5b9d6438b008438bdb54831ca
butcherless <cmarting@gmail.com>
2019-10-15 17:12:36 +0200

    chore(sbt): upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,11 +13,11 @@ object Dependencies {
   lazy val pegdownVersion = "1.6.0"
   lazy val refinedVersion = "0.9.10"
   lazy val scalaLoggingVersion = "3.9.2"
-  lazy val scalazVersion = "7.2.28"
+  lazy val scalazVersion = "7.2.29"
   lazy val scalatestVersion = "3.0.8"
   lazy val slf4jVersion = "1.7.26"
   lazy val specs2Version = "4.7.1"
-  lazy val sttpVersion = "1.7.1"
+  lazy val sttpVersion = "1.7.2"
   lazy val uTestVersion = "0.7.1"
   lazy val zioVersion = "1.0.0-RC14"
 
