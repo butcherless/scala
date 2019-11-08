3ce4096ee24dcba90020aa1b047d40015b33f3d9
butcherless <cmarting@gmail.com>
2019-11-08 15:00:03 +0100

    add queue test operations

--- akka-actors/src/test/scala/com/cmartin/learn/actors/PackageSpec.scala
+++ akka-actors/src/test/scala/com/cmartin/learn/actors/PackageSpec.scala
@@ -1,5 +1,6 @@
 package com.cmartin.learn.actors
 
+import com.cmartin.learn.actors.PackageSpec._
 import org.scalatest.FlatSpec
 
 import scala.collection.mutable.Queue
@@ -35,4 +36,37 @@ class PackageSpec extends FlatSpec {
     info(queue.toString())
     assert(digitSet == queue.toSet)
   }
+
+
+  it should "not remove any processed element" in {
+    val messageSeq = Seq(mnp1, mnp2, mp1, mp2, mp3)
+    var messageQueue: Queue[Message] = Queue.from(messageSeq)
+
+    assert(messageQueue.nonEmpty)
+    // processed removal loop, first message is not processed
+    messageQueue = messageQueue.dropWhile(message => message.processed)
+    assert(messageQueue.size == messageSeq.size)
+  }
+
+  it should "remove the longest prefix (3) of processed elements" in {
+    val messageSeq = Seq(mp1, mp2, mp3, mnp1, mnp2)
+    var messageQueue: Queue[Message] = Queue.from(messageSeq)
+
+    assert(messageQueue.nonEmpty)
+    messageQueue = messageQueue.dropWhile(message => message.processed)
+    assert(messageQueue.size == (messageSeq.size - 3))
+  }
+
+}
+
+object PackageSpec {
+
+  case class Message(payload: Int, processed: Boolean)
+
+  val mnp1 = Message(1, false)
+  val mnp2 = Message(2, false)
+  val mp1 = Message(3, true)
+  val mp2 = Message(4, true)
+  val mp3 = Message(5, true)
+
 }
