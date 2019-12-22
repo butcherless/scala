0a9ecf2d8187bf27f163d15405f110e2255a0807
Carlos Martin <cmarting@gmail.com>
2019-12-22 11:41:02 +0100

    test(zio): add error channel use cases

--- dep-analyzer/src/test/scala/com/cmartin/zio/ZioLearnSpec.scala
+++ dep-analyzer/src/test/scala/com/cmartin/zio/ZioLearnSpec.scala
@@ -8,13 +8,38 @@ import zio._
 
 class ZioLearnSpec extends AnyFlatSpec with Matchers with DefaultRuntime {
 
-  "A Task" should "throw a FiberFailure when an exception occurs" in {
+  "An unfailling UIO effect" should "return a computation" in {
     val program = for {
-      a <- Task(1)
-      b <- Task(a / 0)
-    } yield b
+      r1     <- UIO.effectTotal(0)
+      result <- UIO.effectTotal(r1 + 1)
+    } yield result
+
+    val result = unsafeRun(program)
+
+    result shouldBe 1
+  }
+
+  "A fallible Task effect" should "throw a FiberFailure when an exception occurs" in {
+    val program = for {
+      r1     <- UIO(0)
+      result <- Task(r1 / r1)
+    } yield result
 
     an[FiberFailure] should be thrownBy unsafeRun(program)
+  }
+
+  it should "throw a FiberFailure containing a String when a exception occurs" in {
+    val expectedMessage = "error-message"
+    val program = for {
+      r1     <- UIO(0)
+      result <- Task(r1 / r1).mapError(_ => expectedMessage)
+    } yield result
+
+    val failure = the[FiberFailure] thrownBy unsafeRun(program)
+
+    failure.cause.failureOption.map { message =>
+      message shouldBe expectedMessage
+    }
   }
 
   /* probes the function 'ZIO.either' */
@@ -29,13 +54,14 @@ class ZioLearnSpec extends AnyFlatSpec with Matchers with DefaultRuntime {
     result.swap.map(_ shouldBe an[ArithmeticException])
   }
 
-  it should "refine a failure" in {
+  // Throwable => MyDomainException
+  it should "refine a failure with a custom exception" in {
     import com.cmartin.utils.ZioLearn.refineError
 
     val program: Task[Int] = for {
-      a <- Task.effect(1 + 1)
-      b <- Task.effect(a / 0)
-    } yield b
+      r1     <- Task.effect(0)
+      result <- Task.effect(1 / r1)
+    } yield result
 
     val programRefined: IO[MyDomainException, Int] = program.refineOrDie(refineError())
 
@@ -44,17 +70,17 @@ class ZioLearnSpec extends AnyFlatSpec with Matchers with DefaultRuntime {
     result.swap.map(_ shouldBe a[MyExceptionTwo])
   }
 
-  it should "map an error from None to String" in {
-    val none: Option[Int] = None
-    val noneZio: IO[Unit, Int] = ZIO.fromOption(none)
+  it should "map None to a String into the error channel" in {
+    val none: Option[Int]        = None
+    val noneZio: IO[Unit, Int]   = ZIO.fromOption(none)
     val program: IO[String, Int] = noneZio.mapError(_ => "mapped error")
 
     a[FiberFailure] should be thrownBy unsafeRun(program)
   }
 
   it should "return a Left with an string error" in {
-    val none: Option[Int] = None
-    val noneZio: IO[Unit, Int] = ZIO.fromOption(none)
+    val none: Option[Int]        = None
+    val noneZio: IO[Unit, Int]   = ZIO.fromOption(none)
     val program: IO[String, Int] = noneZio.mapError(_ => "mapped error")
 
     val result: Either[String, Int] = unsafeRun(program.either)
@@ -66,31 +92,46 @@ class ZioLearnSpec extends AnyFlatSpec with Matchers with DefaultRuntime {
     import ZioLearnSpec._
 
     val exclusionList = List("group-2", "group-4")
-    val result = artifacts.filterNot(dep => exclusionList.contains(dep.group))
+    val result        = artifacts.filterNot(dep => exclusionList.contains(dep.group))
 
     result shouldBe filteredArtifacts
   }
 
-  it should "WIP work" in {
-    import zio.console._
+  it should "repeat 6 times exponentially from 10 millis" in {
     import zio.clock._
+    import zio.console._
     import zio.duration._
 
     val policy =
       Schedule.exponential(10.milliseconds) &&
-      //Schedule.spaced(3.seconds)
-    Schedule.recurs(5)
+        //Schedule.spaced(3.seconds)
+        Schedule.recurs(5)
 
     val program: ZIO[Clock with Console, Nothing, (Duration, Int)] = (for {
-    _ <- putStrLn("zio console message")
-    //_ <- sleep(1.second)
+      _ <- putStrLn("zio console message")
+      //_ <- sleep(1.second)
     } yield ()) repeat policy
-
 
     val result: (Duration, Int) = unsafeRun(program)
 
     info(result._1.toMillis.toString)
 
+  }
+
+  it should "compile" in {
+    trait DomainError
+    case class ErrorOne(m: String) extends DomainError
+    case class ErrorTwo(m: String) extends DomainError
+
+    val program: ZIO[Any, DomainError, Int] =
+      for {
+        _      <- Task(1 / 0).mapError(_ => ErrorOne("error-one"))
+        result <- Task(1 / 0).mapError(_ => ErrorTwo("error-two"))
+      } yield result
+
+    val result = unsafeRun(program.either)
+
+    result shouldBe Left(ErrorOne("error-one"))
   }
 }
 
