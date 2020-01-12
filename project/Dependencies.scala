c960f22f96ae6e039712393da703598c6f9f950e
Scala Steward <me@scala-steward.org>
2020-01-12 16:21:03 +0100

    Update utest to 0.7.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   lazy val slf4jVersion        = "1.7.26"
   lazy val specs2Version       = "4.8.3"
   lazy val sttpVersion         = "2.0.0-RC6"
-  lazy val uTestVersion        = "0.7.2"
+  lazy val uTestVersion        = "0.7.3"
   lazy val zioVersion          = "1.0.0-RC17"
   lazy val zioKafkaVersion     = "0.4.1"
 
