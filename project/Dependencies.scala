a45826aad3434258d0bb4212fbab0f596b1aa7cb
Scala Steward <me@scala-steward.org>
2020-03-01 02:06:26 +0100

    Update specs2-core to 4.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   lazy val scalazVersion       = "7.2.30"
   lazy val scalatestVersion    = "3.1.1"
   lazy val slf4jVersion        = "1.7.26"
-  lazy val specs2Version       = "4.8.3"
+  lazy val specs2Version       = "4.9.1"
   lazy val sttpVersion         = "2.0.1"
   lazy val zioVersion          = "1.0.0-RC17"
   lazy val zioConfigVersion    = "1.0.0-RC11"
