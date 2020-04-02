c575a8965c12822b2a8f6b4d0ce475b631516d2a
Scala Steward <me@scala-steward.org>
2020-04-02 20:06:08 +0200

    Update async-http-client-backend-zio, ... to 2.0.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   lazy val slf4jVersion        = "1.7.26"
   lazy val specs2Version       = "4.9.2"
   lazy val zioVersion          = "1.0.0-RC18-2"
-  lazy val sttpVersion         = "2.0.6"
+  lazy val sttpVersion         = "2.0.7"
   lazy val zioConfigVersion    = "1.0.0-RC15"
   lazy val zioKafkaVersion     = "0.4.1"
 
