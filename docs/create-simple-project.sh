9ee7ce226e4ad35a80c6965d0f0ba9f5272b34a6
Carlos Martin <cmarting@gmail.com>
2020-04-23 10:14:11 +0200

    chore: upgrade scala compiler in create project script

--- docs/create-simple-project.sh
+++ docs/create-simple-project.sh
@@ -3,7 +3,7 @@
 PKG_DIR=com/cmartin/learn
 SOURCE_PKG=com.cmartin.learn
 SBT_VER=1.3.9
-SCALA_VER="2.13.1"
+SCALA_VER="2.13.2"
 ASSEMBLY_VER="0.14.10"
 DEP_GRAPH_VER="0.10.0-RC1"
 DEP_UP_VER="1.2.2"
