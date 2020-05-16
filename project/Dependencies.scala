abfe14c154419ab802aee1c8083fdea18a6ef8aa
Carlos Martin <cmarting@gmail.com>
2020-05-16 11:48:53 +0200

    chore: upgrade ZIO

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   lazy val slf4jVersion        = "1.7.26"
   lazy val specs2Version       = "4.9.4"
   lazy val sttpVersion         = "2.1.1"
-  lazy val zioVersion          = "1.0.0-RC18-2"
+  lazy val zioVersion          = "1.0.0-RC19"
   lazy val zioConfigVersion    = "1.0.0-RC17"
   lazy val zioKafkaVersion     = "0.4.1"
 
