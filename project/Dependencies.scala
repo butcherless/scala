d077feaed24fd764580d2bcf41502eac5969ee3b
Scala Steward <me@scala-steward.org>
2020-06-20 04:16:21 +0200

    Update zio to 1.0.0-RC21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,8 +18,8 @@ object Dependencies {
   lazy val slf4jVersion        = "1.7.26"
   lazy val specs2Version       = "4.10.0"
   lazy val sttpVersion         = "2.2.0"
-  lazy val zioVersion          = "1.0.0-RC20"
-  lazy val zioConfigVersion    = "1.0.0-RC20"
+  lazy val zioVersion          = "1.0.0-RC21"
+  lazy val zioConfigVersion    = "1.0.0-RC21"
   lazy val zioKafkaVersion     = "0.4.1"
 
   // production code
