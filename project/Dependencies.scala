d0701702e7e1438f62c57d39e53d048319e6bf43
Carlos Martin <cmarting@gmail.com>
2020-07-01 01:04:53 +0200

    chore: upgrade zio lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   lazy val slf4jVersion        = "1.7.26"
   lazy val specs2Version       = "4.10.0"
   lazy val sttpVersion         = "2.2.1"
-  lazy val zioVersion          = "1.0.0-RC21-1"
+  lazy val zioVersion          = "1.0.0-RC21-2"
   lazy val zioConfigVersion    = "1.0.0-RC23-1"
   lazy val zioKafkaVersion     = "0.4.1"
 
