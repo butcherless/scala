71621ca5a63678c0dd0023728f6912b576bac4dd
Carlos Martin <cmarting@gmail.com>
2020-10-06 12:00:33 +0200

    chore: upgrade libs, zio

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   val scalatest    = "3.2.2"
   val slf4j        = "1.7.26"
   val sttp         = "2.2.9"
-  val zio          = "1.0.1"
+  val zio          = "1.0.2"
   val zioConfig    = "1.0.0-RC27"
   val zioKafka     = "0.12.0"
 
