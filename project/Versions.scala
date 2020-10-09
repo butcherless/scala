77c167c2f8343624af4c8c1e287858e3560be58a
Scala Steward <me@scala-steward.org>
2020-10-09 14:18:43 +0200

    Update akka-actor-typed, ... to 2.6.10

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val akkaHttp     = "10.2.1"
   val akka         = "2.6.10"
-  val akkaTyped    = "2.6.9"
+  val akkaTyped    = "2.6.10"
   val cats         = "2.2.0"
   val circe        = "0.13.0"
   val config       = "1.4.0"
