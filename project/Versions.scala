29f64fb25cac4c11d3d4cdb819ea735271c5751d
Scala Steward <me@scala-steward.org>
2021-01-15 14:15:59 +0100

    Update akka-actor, ... to 2.6.11

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val akkaHttp     = "10.2.2"
-  val akka         = "2.6.10"
-  val akkaTyped    = "2.6.10"
+  val akka         = "2.6.11"
+  val akkaTyped    = "2.6.11"
   val cats         = "2.3.1"
   val circe        = "0.13.0"
   val config       = "1.4.1"
