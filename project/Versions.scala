34b56736b549589170f4beaf7253c84c27f6feed
Scala Steward <me@scala-steward.org>
2021-02-07 02:09:10 +0100

    Update zio-config to 1.0.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,7 +16,7 @@ object Versions {
   val slf4j        = "1.7.26"
   val sttp         = "2.2.9"
   val zio          = "1.0.4-2"
-  val zioConfig    = "1.0.0-RC32"
+  val zioConfig    = "1.0.0"
   val zioKafka     = "0.12.0"
   val zioPrelude   = "1.0.0-RC2"
 }
