321a1ac7373361d98dfac357f0bdf35c6f39f2b6
Carlos Martin <cmarting@gmail.com>
2021-06-02 07:36:11 +0200

    chore: upgrade zio

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   val scalatest    = "3.2.9"
   val slf4j        = "1.7.26"
   val sttp         = "2.2.9"
-  val zio          = "1.0.8"
+  val zio          = "1.0.9"
   val zioConfig    = "1.0.6"
   val zioKafka     = "0.12.0"
   val zioPrelude   = "1.0.0-RC5"
