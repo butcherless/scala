a577588a114a129283a1a0845d0b7a655cc4e83e
Scala Steward <me@scala-steward.org>
2021-09-17 22:42:43 +0200

    Update scalatest to 3.2.10

--- project/Versions.scala
+++ project/Versions.scala
@@ -12,7 +12,7 @@ object Versions {
   val refined = "0.9.27"
   val scalaLogging = "3.9.4"
   val scalaz = "7.3.4"
-  val scalatest = "3.2.9"
+  val scalatest = "3.2.10"
   val slf4j = "1.7.31"
   val sttp = "2.2.10"
   val zio = "2.0.0-M2"
