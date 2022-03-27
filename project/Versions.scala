bd07f42ccbb78f7ec3233e636f84569019e88581
Scala Steward <me@scala-steward.org>
2022-03-27 18:05:58 +0200

    Update zio-prelude to 1.0.0-RC11-2

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,10 +15,10 @@ object Versions {
   val scalatest   = "3.2.11"
   val slf4j       = "1.7.33"
   val sttp        = "3.5.1"
-  val zio         = "2.0.0-RC2"  // RC2 depends on zio-config next release
+  val zio         = "2.0.0-RC2"    // RC2 depends on zio-config next release
   val zioConfig   = "3.0.0-RC3"
   val zioLogging  = "2.0.0-RC5"
   val zioJson     = "0.3.0-RC3"
   val zioKafka    = "0.12.0"
-  val zioPrelude  = "1.0.0-RC10" // RC10 depends on zio-rc2 next release
+  val zioPrelude  = "1.0.0-RC11-2" // RC10 depends on zio-rc2 next release
 }
