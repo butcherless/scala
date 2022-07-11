e0f3aa7466c3e47036df73e15fb687a9771cf932
Carlos Martin <cmarting@gmail.com>
2022-07-11 10:03:50 +0200

    * upgrade sbt tool

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val kafkaClient = "3.2.0"
   val logback     = "1.2.11"
   val pegdown     = "1.6.0"
-  val refined     = "0.10.0"
+  val refined     = "0.10.1"
   val scalaz      = "7.3.6"
   val scalatest   = "3.2.12"
   val slf4j       = "1.7.33"
