b3f3d700f2366984b7fdcde03acfc6482b6bbc4d
Scala Steward <scala_steward@virtuslab.com>
2022-07-21 12:37:27 +0000

    Update client3:core, client3:zio, ... to 3.7.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object Versions {
   val scalaz      = "7.3.6"
   val scalatest   = "3.2.12"
   val slf4j       = "1.7.33"
-  val sttp        = "3.7.0"
+  val sttp        = "3.7.1"
   val zio         = "2.0.0"
   val zioConfig   = "3.0.1"
   val zioLogging  = "2.0.1"
