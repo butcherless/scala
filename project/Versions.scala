35410262bbf5ad383061d9b6f6592f9b39f792bc
Scala Steward <scala_steward@virtuslab.com>
2022-09-13 13:04:26 +0000

    Update zio-logging-slf4j to 2.1.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -17,7 +17,7 @@ object Versions {
   val sttp        = "3.7.6"
   val zio         = "2.0.2"
   val zioConfig   = "3.0.2"
-  val zioLogging  = "2.1.0"
+  val zioLogging  = "2.1.1"
   val zioJson     = "0.3.0-RC11"
   val zioKafka    = "0.12.0"
   val zioPrelude  = "1.0.0-RC15"
