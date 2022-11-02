8e795bddf2292384ec6d0d422ee9a093f06b009b
Scala Steward <scala_steward@virtuslab.com>
2022-11-02 12:39:00 +0000

    Reformat with scalafmt 3.6.1
    Executed command: scalafmt --non-interactive

--- scripts/3g-lte-data.scala
+++ scripts/3g-lte-data.scala
@@ -1,15 +1,15 @@
 import java.time.LocalDate
 
-val MEGA = 1024
-val today = LocalDate.now
+val MEGA       = 1024
+val today      = LocalDate.now
 val dayOfMonth = today.getDayOfMonth
 
 val lastDayOfMonth = LocalDate.now.getMonth.maxLength
-val limit = 100 * dayOfMonth.toDouble / lastDayOfMonth
+val limit          = 100 * dayOfMonth.toDouble / lastDayOfMonth
 
 val mBytesPerDay = 2.toDouble * MEGA / lastDayOfMonth
 
 val message = "today: " + today + " - " +
-               f"$limit%2.2f"+ "% - " + f"$mBytesPerDay%2.2f" + " MBytes/day"
+  f"$limit%2.2f" + "% - " + f"$mBytesPerDay%2.2f" + " MBytes/day"
 
 println(message)
