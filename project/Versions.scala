54ec112a5a80410fdd93f63dd0bc1c80e1a37baf
Carlos Martin <cmarting@gmail.com>
2022-12-24 11:19:03 +0100

    * upgrade zio logging

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,7 +16,7 @@ object Versions {
   val sttp        = "3.8.5"
   val zio         = "2.0.5"
   val zioConfig   = "3.0.6"
-  val zioLogging  = "2.1.5"
+  val zioLogging  = "2.1.6"
   val zioKafka    = "0.12.0"
   val zioPrelude  = "1.0.0-RC16"
 }
