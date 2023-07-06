a4a6d743463c7247505453ecb76a41ff2371e10e
Carlos Martin <cmarting@gmail.com>
2023-07-06 12:34:05 +0200

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val akkaHttp    = "10.5.2"
-  val akka        = "2.8.2"
-  val akkaTyped   = "2.8.2"
+  val akka        = "2.8.3"
+  val akkaTyped   = "2.8.3"
   val cats        = "2.9.0"
   val circe       = "0.14.5"
   val config      = "1.4.2"
