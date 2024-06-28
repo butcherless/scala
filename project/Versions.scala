bb9bb2033a3a1c69c65e78ddd3fe5b6bb69f9bf5
Carlos Martin <cmarting@gmail.com>
2024-06-28 10:24:44 +0200

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object Versions {
   val scalaz      = "7.3.8"
   val scalatest   = "3.2.19"
   val sttp        = "3.9.7"
-  val zio         = "2.1.4"
+  val zio         = "2.1.5"
   val zioConfig   = "4.0.2"
   val zioLogging  = "2.3.0"
   val zioKafka    = "0.12.0"
