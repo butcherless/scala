b572550f557cbf74bfe7aee17646df4569252b97
Scala Steward <scala_steward@virtuslab.com>
2024-08-05 16:29:00 +0000

    Update zio to 2.1.7

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object Versions {
   val scalaz      = "7.3.8"
   val scalatest   = "3.2.19"
   val sttp        = "3.9.7"
-  val zio         = "2.1.6"
+  val zio         = "2.1.7"
   val zioConfig   = "4.0.2"
   val zioLogging  = "2.3.0"
   val zioKafka    = "0.12.0"
