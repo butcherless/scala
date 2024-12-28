ed7d853a9e252250a3af6c350d10b8c9976724d3
Carlos Martin <cmarting@gmail.com>
2024-12-28 23:36:34 +0100

    fix prelude regression

--- zio-prelude/src/test/scala/com/cmartin/learn/Commons.scala
+++ zio-prelude/src/test/scala/com/cmartin/learn/Commons.scala
@@ -1,11 +1,11 @@
 package com.cmartin.learn
 import org.scalatest.Assertions.fail
-import zio.prelude.fx.Cause
+import zio.Cause
 
 object Commons {
   def causeToErrorList[E, A](either: Either[Cause[E], A]) = {
     either match {
-      case Left(cause)  => Left(cause.toChunk.toList)
+      case Left(cause)  => Left(cause.failures)
       case Right(value) => fail(s"result should be Left: $value")
     }
   }
