1275a47889da4d62cd97809a8308a4916bf7abfe
Scala Steward <scala_steward@virtuslab.com>
2025-01-22 14:57:22 +0000

    Update cats-core, cats-free to 2.13.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val akkaHttp    = "10.5.3"
   val akka        = "2.8.8"
   val akkaTyped   = "2.8.8"
-  val cats        = "2.12.0"
+  val cats        = "2.13.0"
   val circe       = "0.14.10"
   val config      = "1.4.3"
   val figlet4s    = "0.3.2"
