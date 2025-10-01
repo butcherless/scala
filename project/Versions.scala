e461eaa909dfa344de25740ffb3b44f56dd34c88
Scala Steward <scala_steward@virtuslab.com>
2025-10-01 14:27:54 +0000

    Update circe-generic, circe-parser to 0.14.15

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val akka        = "2.8.8"
   val akkaTyped   = "2.8.8"
   val cats        = "2.13.0"
-  val circe       = "0.14.14"
+  val circe       = "0.14.15"
   val config      = "1.4.4"
   val figlet4s    = "0.3.2"
   val json4s      = "4.0.7"
