fe20ef7b376886d16a2084900241b4a91c4662e1
Scala Steward <scala_steward@virtuslab.com>
2025-12-22 19:19:12 +0000

    Update logback-classic to 1.5.23

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val figlet4s    = "0.3.2"
   val json4s      = "4.1.0"
   val justSemver  = "1.1.1"
-  val logback     = "1.5.22"
+  val logback     = "1.5.23"
   val kafkaClient = "4.1.1"
   val refined     = "0.11.3"
   val scalaz      = "7.3.8"
